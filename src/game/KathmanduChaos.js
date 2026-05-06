e493037fa9d4fae23a877f1bc7feb94db7023f6b
dipenlama <lamadipen@yahoo.com>
2026-05-05 22:45:44 -0400

    Modified the loading screen

--- src/game/KathmanduChaos.js
+++ src/game/KathmanduChaos.js
@@ -218,7 +218,7 @@ export class KathmanduChaos {
     this.setupGarage();
     this.loadLevel(0, { showIntro: false });
     await this.waitForMinimumLoadingTime(bootStarted);
-    this.showTitleMenu();
+    this.showGarage();
     this.animate();
   }
 
